68dd4dd50db6d5fd21876d6613ce2d3f5ba9bc21
EnricoFH <enricofh@gmail.com>
2026-06-06 19:13:23 -0300

    Sincronizando

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
+import  minasHome from '../assets/minasHome.png';
+import './Home.css';
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
+
 
 const Home = () => {
   return (
     <div>
-      <Header />
-      <h1>Home</h1>
-      <Footer />
+
+      <div className="banner">
+        <img src={minasHome} alt="Imagem de mulheres scientistas" className="banner-img" />
+        <div className="banner-text">
+            <p className="banner-subtitle">- Jornada Minatech . Florianópolis</p>
+            <h1 className="banner-title">Tecnologia feita <em>por meninas,<br />para meninas.</em></h1>
+        </div>  
+      </div>
     </div>
   );
 };
