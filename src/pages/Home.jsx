ab2c4054a220ccc4b95a9c904100be058754b755
EnricoFH <enricofh@gmail.com>
2026-06-09 17:55:28 -0300

    home base pronta

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,6 +34,16 @@ const Home = () => {
           <div className="circulo-deco c4"></div>
         </div>  
       </section>
+      <section className="secao-quem-somos">
+        <div className="quem-somos-texto">
+          <h2 className="quem-somos-titulo">Quem somos</h2>
+          <h3 className="quem-somos-subtitulo">Corali e Engenheiros sem Fronteiras — núcleo Florianópolis</h3>
+          <hr className="quem-somos-divisor" />
+          <p>A Corali e os Engenheiros sem Fronteiras (Florianópolis), se uniram para organizar a Jornada Minatech.</p>
+          <p>Temos o sonho de inspirar meninas a conhecerem e seguirem carreiras tecnológicas, mostrando que elas têm capacidade e que essas profissões não são só para meninos.</p>
+          <p>Para isso, promovemos oficinas de lógica, matemática e física, junto com talks sobre profissões, soft skills, empreendedorismo, diversidade e empoderamento feminino.</p>
+        </div>
+      </section>
     </div>
   );
 };
