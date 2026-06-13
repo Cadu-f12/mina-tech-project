cb960433da7cd1096c029b0bd6ecc58307eb2a91
Cadu-f12 <cadudailha68@gmail.com>
2026-06-13 15:39:32 -0300

    feat: Create a mapping data to banners

--- src/components/programacao/former-student/FormerStudentSection.jsx
+++ src/components/programacao/former-student/FormerStudentSection.jsx
@@ -1,9 +1,7 @@
 import "./FormerStudentSection.css";
 
-import exAluna1 from "../../../assets/ex-aluna1.png";
-import exAluna2 from "../../../assets/ex-aluna2.png";
-
 import FormerStudent from "./FormerStudent";
+import { formerStudents } from "./data-student";
 
 export default function FormerStudentSection() {
     return (
@@ -11,26 +9,16 @@ export default function FormerStudentSection() {
             <section className="former-student-section">
                 <h2>Depoimentos de ex-alunas</h2>
 
-                <FormerStudent
-                    image={exAluna1}
-                    title="Vitória da Rosa"
-                    text="
-                    Minha jornada no Minatech, em 2022 e 2023, foi uma grande fonte de inspiração e crescimento.
-                    O programa me ajudou a descobrir caminhos na Engenharia e Tecnologia, contribuindo para minha formação em Engenharia de Software e para meu desenvolvimento profissional.
-                    Sou muito grata ao Minatech pelas oportunidades e por incentivar mulheres a irem cada vez mais longe.
-                    "
-                />
+                {formerStudents.map((student) => (
+                    <FormerStudent
+                        key={student.id}
+                        image={student.image}
+                        title={student.title}
+                        text={student.text}
+                        reverse={student.reverse}
+                    />
+                ))}
 
-                <FormerStudent
-                    image={exAluna2}
-                    title="Maria Eduarda"
-                    text="
-                    Participar do projeto entre 2020 e 2022 transformou minha visão de futuro e fortaleceu minha confiança na programação.
-                    Hoje, cursando Ciência da Computação, reconheço o quanto essa experiência contribuiu para minha base acadêmica e profissional.
-                    Sou grata por iniciativas como essa, que incentivam mais mulheres a conquistarem espaço e crescerem na tecnologia.
-                    "
-                    reverse
-                />
             </section>
         </>
     );
--- src/components/programacao/past-schedule/PastScheduleSection.jsx
+++ src/components/programacao/past-schedule/PastScheduleSection.jsx
@@ -1,34 +1,24 @@
-import "./PastScheduleSection.css"
-
-import foto1 from "../../../assets/pesquisadoras.png";
-import foto2 from "../../../assets/mulher-hardware.png";
+import "./PastScheduleSection.css";
 
 import PastSchedule from "./PastSchedule";
-
-// image, text, reverse
+import { pastScheduleBanners } from "./data-past";
 
 export default function PastScheduleSection() {
     return (
         <>
             <section className="past-schedule-section">
                 <h2>Programações Passadas</h2>
-                <PastSchedule
-                    image={foto1}
-                    text="
-                    Nossa imersão no Laboratório de Química do IFSC, conduzida pelas professoras Berenice e Gisele, foi uma vitrine sobre a versatilidade da Engenharia Química. 
-                    Além de explorarmos as áreas de atuação e as crescentes oportunidades do mercado, vivenciamos a prática da transformação e o descarte correto de efluentes. 
-                    "
-                />
-                <PastSchedule
-                    image={foto2}
-                    text="
-                    Com a participação especial do Time Curie, do Senai, e da professora Daniela Suzuki, acompanhada por suas alunas graduandas e doutorandas da UFSC, 
-                    vivenciamos uma troca de experiências. 
-                    As atividades mostraram compartilhamento de trajetórias reais e conhecimentos técnicos avançados nas áreas de engenharia e tecnologia. 
-                    "
-                    reverse
-                />
+
+                {pastScheduleBanners.map((banner) => (
+                    <PastSchedule
+                        key={banner.id}
+                        image={banner.image}
+                        text={banner.text}
+                        reverse={banner.reverse}
+                    />
+                ))}
+                
             </section>
         </>
     );
-} 
+}
