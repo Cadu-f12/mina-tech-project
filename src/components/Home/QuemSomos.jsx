197a2060a79981f9a945dc67e3809e920d73d722
ENRICO DE FRANCESCHI HOEFEL <enrico_hoefel@estudante.sesisenai.org.br>
2026-06-16 21:05:17 -0300

    só falta responsividade

--- src/components/Home/QuemSomos.jsx
+++ src/components/Home/QuemSomos.jsx
@@ -4,15 +4,17 @@ import suaImagem from '../../assets/Equipe.png';
 const QuemSomos = () => {
   return (
     <section className="secao-quem-somos">
-      <img src={suaImagem} alt="Foto da equipe" className="quem-somos-img" />
-      <div className="quem-somos-texto">
-        <h2 className="quem-somos-titulo">Quem somos</h2>
-        <h3 className="quem-somos-subtitulo">Corali e Engenheiros sem Fronteiras — núcleo Florianópolis</h3>
-        <hr className="quem-somos-divisor" />
-        <p>A Corali e os Engenheiros sem Fronteiras (Florianópolis), se uniram para organizar a Jornada Minatech.</p>
-        <p>Temos o sonho de inspirar meninas a conhecerem e seguirem carreiras tecnológicas, mostrando que elas têm capacidade e que essas profissões não são só para meninos.</p>
-        <p>Para isso, promovemos oficinas de lógica, matemática e física, junto com talks sobre profissões, soft skills, empreendedorismo, diversidade e empoderamento feminino.</p>
-      </div>
+        <div className="quem-somos-texto">
+            <h2 className="quem-somos-titulo">Quem somos</h2>
+            <h3 className="quem-somos-subtitulo">Corali e Engenheiros sem Fronteiras — núcleo Florianópolis</h3>
+            <hr className="quem-somos-divisor" />
+        </div>
+        <img src={suaImagem} alt="Foto da equipe" className="quem-somos-img" />
+        <div className="quem-somos-texto">
+            <p>A Corali e os Engenheiros sem Fronteiras (Florianópolis), se uniram para organizar a Jornada Minatech.</p>
+            <p>Temos o sonho de inspirar meninas a conhecerem e seguirem carreiras tecnológicas, mostrando que elas têm capacidade e que essas profissões não são só para meninos.</p>
+            <p>Para isso, promovemos oficinas de lógica, matemática e física, junto com talks sobre profissões, soft skills, empreendedorismo, diversidade e empoderamento feminino.</p>
+        </div>
     </section>
   );
 };
